dea04dde85e0bc80423098a7e2e95ebea1c2d09d
Oleg Hnoievets <oleh.hnoievets@cleveroad.com>
2024-11-29 10:13:31 +0200

    Updated files for deployment

--- monitor-api.sh
+++ monitor-api.sh
@@ -2,11 +2,11 @@
 
 start() {
   cd /opt/monitor_api
-  ./counter > /dev/null 2> /dev/null &
+  ./counter --start > /dev/null 2> /dev/null &
 }
 
 stop() {
-  kill -s SIGKILL `ps -ef | grep -i "./counter" | awk '{print $2;}'`
+  kill -s SIGKILL `ps -ef | grep -i "./counter --start" | awk '{print $2;}'`
 }
 
 case $1 in
--- setup.sh
+++ setup.sh
@@ -6,11 +6,9 @@ SERVICE="monitor-api.service"
 DEPLOY_PATH="/opt/monitor_api"
 
 sudo mkdir $DEPLOY_PATH
+sudo chown -R $USER:$USER $DEPLOY_PATH
 cp * $DEPLOY_PATH
 cd $DEPLOY_PATH
-
-sudo ~/nginx_deploy_virtual_hosts.sh counter.com
-cp ./index.html /var/www/counter.com/html
 
 echo "Building app..."
 
@@ -19,16 +17,16 @@ go build -o counter
 
 echo "Built"
 echo "Creating db..."
+
 ./counter --createdb
 
 echo "Starting service..."
 
-cp ./$SERVICE /etc/systemd/system/$SERVICE
+sudo cp ./$SERVICE /etc/systemd/system/$SERVICE
 sudo chmod +x ./monitor-api.sh
 
 sudo systemctl enable $SERVICE
 sudo systemctl start $SERVICE
 sudo systemctl status $SERVICE
-
 
 echo "Deployed successfully!"
